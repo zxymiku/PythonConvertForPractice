01ea1f506cf82a2966a872a3813a6595d23222c0
zxymiku <zxymiku@outlool.com>
2024-02-23 22:44:04 +0800

    16

---
+++ 16ehx.py
@@ -0,0 +1,12 @@
+import base64
+
+# 输入的Base64编码字符串
+base64_encoded_string = input("请输入Base64编码字符串:")
+
+# 解码Base64编码字符串
+decoded_bytes = base64.b64decode(base64_encoded_string)
+
+# 将解码后的字节表示转换为16进制
+hex_representation = decoded_bytes.hex()
+
+print("16进制表示:", hex_representation)
