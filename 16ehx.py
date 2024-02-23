6d317f1e11442d48b874e519b45d64de8c4a972a
zxymiku <zxymiku@outlook.com>
2024-02-23 23:15:50 +0800

    16

--- 16ehx.py
+++ 16ehx.py
@@ -6,7 +6,7 @@ base64_encoded_string = input("请输入Base64编码字符串:")
 # 解码Base64编码字符串
 decoded_bytes = base64.b64decode(base64_encoded_string)
 
-# 将解码后的字节表示转换为1
+# 将解码后的字节表示转换为
 hex_representation = decoded_bytes.hex()
 
 print("16进制表示:", hex_representation)
