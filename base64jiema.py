9c044ed9199bbc19720e51c52d8d325d3003f79d
zxymiku <95949338+zxymiku@users.noreply.github.com>
2024-03-16 16:37:13 +0800

    更新 base64jiema.py

--- base64jiema.py
+++ base64jiema.py
@@ -3,4 +3,6 @@ import base64
 encoded_text = input("请输入base64编码")
 decoded_text = base64.b64decode(encoded_text).decode('utf-8')
 
+base.hashmd5 = string(input=("md5编码"))
+base64.b64decode(hashmd5).del('utf-8')
 print(decoded_text)
